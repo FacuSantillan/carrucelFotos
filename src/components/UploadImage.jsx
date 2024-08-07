9e55096a7adfdd08d67da5dcd839defef26dbca3
Facundo <facusantillan1@gmail.com>
2024-08-06 22:20:23 -0300

    eaf

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
 import { storage } from '../firebase';
+import texto from '../texto.png';
+import foto from '../imagen.png';
 
 const UploadImage = ({ setImages }) => {
   const [preview, setPreview] = useState(null);
+  const [isImageSelected, setIsImageSelected] = useState(false);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -11,6 +15,7 @@ const UploadImage = ({ setImages }) => {
       const reader = new FileReader();
       reader.onloadend = () => {
         setPreview(reader.result);
+        setIsImageSelected(true); // Cambia el estado al seleccionar una imagen
       };
       reader.readAsDataURL(file);
     }
@@ -23,6 +28,9 @@ const UploadImage = ({ setImages }) => {
         getDownloadURL(imageRef).then(url => {
           setImages(prevImages => [...prevImages, url]);
           setPreview(null); // Clear the preview after upload
+          setIsImageSelected(false); // Restablece el estado después de cargar la imagen
+          setUploadSuccess(true); // Muestra el mensaje de éxito
+          setTimeout(() => setUploadSuccess(false), 3000); // Oculta el mensaje después de 3 segundos
         });
       }).catch(error => console.log(error));
     }
@@ -30,6 +38,9 @@ const UploadImage = ({ setImages }) => {
 
   return (
     <div className="upload-container">
+      <img src={texto} className='texto-imagen'/>
+      <img src={foto} className='imagen'/>
+      <h2 className='texto'>Comparte tus momentos!</h2>
       <input
         type="file"
         accept="image/*"
@@ -37,16 +48,24 @@ const UploadImage = ({ setImages }) => {
         style={{ display: 'none' }}
         id="fileInput"
       />
-      <button onClick={() => document.getElementById('fileInput').click()}>
-        Seleccionar Foto
+      <button onClick={() => {
+        if (isImageSelected) {
+          handleUpload();
+        } else {
+          document.getElementById('fileInput').click();
+        }
+      }}>
+        {isImageSelected ? 'Publicar' : 'Seleccionar Foto'}
       </button>
       {preview && (
-        <>
-          <div className="preview-container">
-            <img src={preview} alt="Image Preview" className="image-preview" />
-          </div>
-          <button onClick={handleUpload}>Publicar</button>
-        </>
+        <div className="preview-container">
+          <img src={preview} alt="Image Preview" className="image-preview" />
+        </div>
+      )}
+      {uploadSuccess && (
+        <div className="success-message">
+          ¡Imagen subida con éxito!
+        </div>
       )}
     </div>
   );
